27e20527049764da40b9aae18b1fae77c5496498
Kyle <kylegranthousel@gmail.com>
2025-09-08 14:02:28 -0400

    Built basic signup > sign in > sign out flow

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+  authenticated :user do
+    root to: "students#index", as: :authenticated_root   # pick a real page you have
+  end
+
   devise_scope :user do
-    root to: "devise/sessions#new"
+    unauthenticated do
+      root to: "devise/sessions#new", as: :unauthenticated_root
+    end
   end
 end
