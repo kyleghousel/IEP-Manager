baa92f6caead62e6d3e14370c9bbd24f44420043
Kyle <kylegranthousel@gmail.com>
2025-09-10 21:29:23 -0400

    Adjust note length validation

--- app/models/student_accommodation.rb
+++ app/models/student_accommodation.rb
@@ -2,5 +2,5 @@ class StudentAccommodation < ApplicationRecord
   belongs_to :student
   belongs_to :accommodation
 
-  validates :notes, length: { minimum: 40 }
+  validates :notes, length: { minimum: 20 }
 end
