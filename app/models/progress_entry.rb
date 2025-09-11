7bfbe8ef290c090e824e0398d90e9b49dcffa727
Kyle <kylegranthousel@gmail.com>
2025-09-10 20:38:56 -0400

    Add validations

--- app/models/progress_entry.rb
+++ app/models/progress_entry.rb
@@ -1,4 +1,13 @@
 class ProgressEntry < ApplicationRecord
   belongs_to :student_goal
   belongs_to :user
+
+  validates :recorded_on, :note, presence: true
+  validates :note, length: { in: 30..250 }
+  validates :score_numeric, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 0,
+    less_than_or_equal_to: 100,
+    allow_nil: true,
+  }
 end
