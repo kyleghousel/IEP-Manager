d6cdce9d09afab472dd3a403a69eb00a2ab385d8
Kyle <kylegranthousel@gmail.com>
2025-09-26 11:14:24 -0400

    Remove logs and connect method

--- app/javascript/controllers/confirm_controller.js
+++ app/javascript/controllers/confirm_controller.js
@@ -3,13 +3,8 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static values = { message: String }
 
-  connect() { console.log("confirm#connect", this.element) }
-
   confirm(event) {
-    console.log('anything?')
-    console.log(event)
     if (!window.confirm(this.messageValue)) {
-      console.log(this.messageValue)
       event.preventDefault()
     }
   }
